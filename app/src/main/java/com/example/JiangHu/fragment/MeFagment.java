32d07fc2f8f9a453bcad3eaa74d9158687aa9b30
christ-shaw <christ_shaw@hotails.com>
2017-02-10 22:33:25 +0800

    修改个人中心

--- app/src/main/java/com/example/JiangHu/fragment/MeFagment.java
+++ app/src/main/java/com/example/JiangHu/fragment/MeFagment.java
@@ -11,22 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.SimpleAdapter;
 
 import com.example.JiangHu.Constant;
 import com.example.JiangHu.MainActivity;
 import com.example.JiangHu.MyProfileActivity;
-<<<<<<< Updated upstream
 import com.example.JiangHu.MyTaskAdapter;
 import com.example.JiangHu.R;
-=======
 import com.example.JiangHu.grid.MyGridAdapter;
->>>>>>> Stashed changes
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -39,15 +30,6 @@ public class MeFagment extends Fragment {
     private int[] pic_path = {R.drawable.completed, R.drawable.ongoing, R.drawable.published, R.drawable.evaluated};
     private String[] texts = {"已完成","进行中","我发布的","待评价"};
 
-<<<<<<< Updated upstream
-=======
-
-    private LinearLayout ll_user_life;
-    private LinearLayout ll_user_members;
-    private LinearLayout ll_user_store;
-    private LinearLayout ll_user_opinion;
-
->>>>>>> Stashed changes
     private ImageView myprofiles;
 
 
@@ -63,8 +45,6 @@ public class MeFagment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         MainActivity activity = (MainActivity) getActivity();
-        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
-        toolbar.setVisibility(View.GONE);
     }
     @Override
     public void onDestroyView() {
@@ -95,35 +75,15 @@ public class MeFagment extends Fragment {
         });
 
 
-<<<<<<< Updated upstream
-        my_gridView_user = (GridView) view.findViewById(R.id.gridview);
-
-        List<Map<String, Object>> lstImageItem = new ArrayList<Map<String, Object>>();
-        for(int i = 0 ; i < pic_path.length; ++i)
-        {
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("taskImage",pic_path[i]);//添加图像资源的ID
-            map.put("taskText", texts[i]);//按序号做ItemText
-            lstImageItem.add(map);
-        }
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),lstImageItem,R.layout.item_task_status,
-                                 new String[] {"taskImage","taskText"},
-                                 new int[]{R.id.taskImage,R.id.taskText});
-        my_gridView_user.setAdapter(simpleAdapter);
-=======
         my_gridView_user = (GridView) view.findViewById(R.id.static_grid_view);
 
-        MyGridAdapter adapter = new MyGridAdapter(getContext());
-        adapter.setImg_text(texts);
-        adapter.setImgs(pic_path);
-        my_gridView_user.setAdapter(adapter);
+        MyGridAdapter gridAdapter = new MyGridAdapter(getContext());
+        gridAdapter.setImg_text(texts);
+        gridAdapter.setImgs(pic_path);
         my_gridView_user.setNumColumns(texts.length);
 
-        my_gridView_user.setAdapter(adapter);
+        my_gridView_user.setAdapter(gridAdapter);
 
-
->>>>>>> Stashed changes
 
     }
 
